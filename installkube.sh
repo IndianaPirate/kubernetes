c61d8cf4d5f160243ea43f89b2438e66515f6ee8
NIAINA Andriharisoa <andriharisoaniaina@gmail.com>
2023-08-24 15:46:47 +0200

    Update installkube.sh

--- installkube.sh
+++ installkube.sh
@@ -2,21 +2,6 @@
 
 GREEN='\033[0;32m'
 config=$(hostname)
-function create_kube_directory(){
-  if [ $config == "master" ]; then
-    echo -e "${GREEN}CREATE .KUBE DIRECTORY${GREEN}"
-    mkdir -p ~/.kube
-    sudo cp -i /etc/kubernetes/admin.conf ~/.kube/config
-    sudo chown -R vagrant:vagrant ~/.kube/
-  fi
-}
-
-function install_weave_net(){
-  if [ $config == "master" ]; then
-    echo -e "${GREEN}INSTALL WEAVE NET${GREEN}"
-    kubectl apply -f https://github.com/weaveworks/weave/releases/download/v2.8.1/weave-daemonset-k8s.yaml
-  fi
-}
 
 function install_kubectl_kubeadm_kubelet(){
   echo -e "${GREEN}INSTALL KUBEADM KUBELET KUBECTL${GREEN}"
