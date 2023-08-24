64098926ce57217c9fa9f0115e2d91d0c6156a30
NIAINA Andriharisoa <andriharisoaniaina@gmail.com>
2023-08-24 15:11:31 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -50,7 +50,7 @@ function modules(){
   cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
   overlay
   br_netfilter
-  EOF
+EOF
   
   sudo modprobe overlay
   sudo modprobe br_netfilter
@@ -60,7 +60,7 @@ function modules(){
   net.bridge.bridge-nf-call-iptables  = 1
   net.bridge.bridge-nf-call-ip6tables = 1
   net.ipv4.ip_forward                 = 1
-  EOF
+EOF
 
   # Apply sysctl params without reboot
   sudo sysctl --system
