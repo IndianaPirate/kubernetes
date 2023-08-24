68222be4cc06307a4e8e5e6dfdcd72db5f23c8df
NIAINA Andriharisoa <andriharisoaniaina@gmail.com>
2023-08-24 14:47:56 +0200

    add new update

--- installkube.sh
+++ installkube.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 config=$(hostname)
-function create_kube(){
+function create_kube_directory(){
   if [ $config == "master" ]; then
     mkdir -p ~/.kube
     sudo cp -i /etc/kubernetes/admin.conf ~/.kube/config
@@ -10,7 +10,9 @@ function create_kube(){
 }
 
 function install_weave_net(){
-  kubectl apply -f https://github.com/weaveworks/weave/releases/download/v2.8.1/weave-daemonset-k8s.yaml
+  if [ $hostnam == "master" ]; then
+    kubectl apply -f https://github.com/weaveworks/weave/releases/download/v2.8.1/weave-daemonset-k8s.yaml
+  fi
 }
 
 function install_kubectl_kubeadm_kubelet(){
@@ -24,9 +26,11 @@ function install_kubectl_kubeadm_kubelet(){
   sudo apt-get install -y kubelet=1.28.0-00 kubeadm=1.28.0-00 kubectl=1.28.0-00 
   sudo apt-mark hold kubelet kubeadm kubectl
 }
+# Install kubectl kubeadm kubelet
 install_kubectl_kubeadm_kubelet
-create_kube
 
-if [ $config == "master" ]; then
-  install_weave_net
-fi
+# Create .kube directory
+create_kube_directory
+
+# Install weave net in master node only
+install_weave_net
