ccf71dabf1b1da2a3fa920b219c3ea03c982c823
NIAINA Andriharisoa <andriharisoaniaina@gmail.com>
2023-08-24 14:13:39 +0200

    configuration firewall

--- install.sh
+++ install.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
+sudo apt update -y
+sudo apt install ufw -y
+sudo ufw enable
+hostnam=$(hostname)
+if [ hostnam == "master" ]; then
+  sudo ufw allow 6443/tcp
+  sudo ufw allow 2379:2380/tcp
+  sudo ufw allow 10250/tcp
+  sudo ufw allow 10251/tcp
+  sudo ufw allow 10252/tcp
+  sudo ufw allow 6783/tcp
+else
+  sudo ufw allow 10250/tcp
+  sudo ufw allow 30000:32767/tcp
+  sudo ufw allow 6783/tcp
+fi
+
 cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
 overlay
 br_netfilter
